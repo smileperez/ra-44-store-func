a9d70a432fafec9da51f09fbd94e3e102c170ea1
smileperez <aa.efimenko@yandex.ru>
2023-03-08 23:48:00 +0300

    first commit

---
+++ src/components/ShopItemFunc.js
@@ -0,0 +1,30 @@
+export default ShopItemFunc;
+
+function ShopItemFunc(props) {
+    const {item} = props;
+
+    return (
+        <div className="container">
+          <div className="background-element"></div>
+          <div className="highlight-window">
+            <div className='highlight-overlay'></div>
+          </div>
+          <div className="window">
+            <div class="main-content">
+                <h2>{item.brand}</h2>
+                <h1>{item.title}</h1>
+                <h3>{item.description}</h3>
+                <div class="description">
+                  {item.descriptionFull} 
+                </div>
+                <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+                <div class="divider"></div>
+                <div class="purchase-info">
+                    <div class="price">{item.currency}{item.price}</div>
+                    <button>Добавить в корзину</button>
+                </div>
+                </div>
+          </div>
+        </div>
+    )
+}
